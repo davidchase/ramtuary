810e470726f3336a0cc72443a95f4401d6a05045
David Annopolsky <dannopolsky@freepeople.com>
2015-09-23 17:53:35 -0400

    adjust compose and change link shorterner to use Futures

--- examples/function/useWith.js
+++ examples/function/useWith.js
@@ -1,11 +1,11 @@
-var double = y => y * 2;
-var square = x => x * x;
-var add = (a, b) => a + b;
+const double = y => y * 2;
+const square = x => x * x;
+const add = (a, b) => a + b;
 // Adds any number of arguments together
-var addAll = (...args) => R.reduce(add, 0, args);
+const addAll = (...args) => R.reduce(add, 0, args);
 
 // Basic example
-var addDoubleAndSquare = R.useWith(addAll, double, square);
+const addDoubleAndSquare = R.useWith(addAll, double, square);
 
 //≅ addAll(double(10), square(5));
 addDoubleAndSquare(10, 5); //=> 45
--- examples/list/map.js
+++ examples/list/map.js
@@ -1,3 +1,3 @@
-var double = x => x * 2;
+const double = x => x * 2;
 
 R.map(double, [1, 2, 3]);
