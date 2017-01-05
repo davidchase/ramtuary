90975c13f1b29a7e8c863bc66013ab6f9512d221
David Chase <davidchase03@gmail.com>
2017-01-05 00:33:38 -0500

    update future
    update future to use working url... prefer map fusion via composition and using invoker to promote functional over fluent.. closes #12

--- examples/fantasy/futures.js
+++ examples/fantasy/futures.js
@@ -1,19 +1,24 @@
-const log = console.log.bind(console);
-const error = console.error.bind(console);
+const log = console.log
+const error = console.error
 
-const url = 'http://reqres.in/api/users?page=3';
+const url = 'https://reqres.in/api/users?page=3'
 
 const xhr = function(url) {
     return new Future(function(rej, res) {
-        const oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", res, false);
-        oReq.addEventListener("error", rej, false);
-        oReq.addEventListener("abort", rej, false);
-        oReq.open("get", url, true);
-        oReq.send();
+        const oReq = new XMLHttpRequest()
+        oReq.addEventListener("load", res, false)
+        oReq.addEventListener("error", rej, false)
+        oReq.addEventListener("abort", rej, false)
+        oReq.open("get", url, true)
+        oReq.send()
     });
 };
 
-const f = R.compose(R.map(S.parseJson), R.map(R.path(['target', 'response'])), xhr)(url);
+const future = R.map(R.compose(S.fromMaybe({err:'Something went wrong'}), 
+                               S.parseJson(Object),
+                               R.path(['target', 'response'])))
 
-f.fork(error, R.map(log)); //=> Object {page: "3", per_page: 3, total: 12, total_pages: 4, data: Array[3]}
+const fork = R.invoker(2, 'fork')
+const run = compose(fork(error, log), future, xhr)
+
+run(url)
